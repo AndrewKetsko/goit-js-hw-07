be2ac9c6d25af0a33383d1884ae2dccfa1536e2f
Andrew Ketsko <monicman82@gmail.com>
2023-01-28 20:29:54 +0200

    summary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryItemsToInsert = galleryItems.map(({ preview, original, description }) =>
+    `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`).join('');
+
+const placeToInsertGallery = document.querySelector('.gallery');
+placeToInsertGallery.innerHTML = galleryItemsToInsert;
+
+placeToInsertGallery.addEventListener('click', allRestMagic);
+
+function allRestMagic(event) {
+  event.preventDefault();
+        
+    if (!event.target.classList.contains('gallery__image')) { return };
+    
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  window.addEventListener('keydown', escBtnClicked);
+
+  function escBtnClicked (event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', escBtnClicked);
+    };
+  };
+};
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsToInsert = galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__item" href=${original}>
+    <img class="gallery__image" src=${preview} alt='${description}' title='${description}'/>
+    </a>`).join('');
+
+const placeToInsertGallery = document.querySelector('.gallery');
+placeToInsertGallery.innerHTML = galleryItemsToInsert;
+
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250,/* options */ });
+
